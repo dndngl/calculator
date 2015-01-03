0f73ce142f8ab5162221aa7ec3e3236fd60f92c4
Kim <cckdycc@naver.com>
2015-01-03 12:33:15 +0900

    초기 계산기 코드

---
+++ src/com/study/main/Add.java
@@ -0,0 +1,7 @@
+package com.study.main;
+
+public class Add {
+	
+	
+
+}
---
+++ src/com/study/main/calculator.java
@@ -0,0 +1,17 @@
+package com.study.main;
+
+public class calculator {
+	
+	public static void main(String[] args){
+
+		int num1=6;
+		int num2=2;
+		
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1/num2);
+		System.out.println(num1*num2);
+		
+	
+	}
+}
