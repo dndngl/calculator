112cd76a1feaed0ad5c78234c09576be9db8a4d3
Kim <cckdycc@naver.com>
2015-01-03 21:10:16 +0900

    코드 완전 수정

--- src/com/study/main/Calculator.java
+++ src/com/study/main/Calculator.java
@@ -1,39 +1,43 @@
 package com.study.main;
-
 import java.util.Scanner;
 
-public class calculator {
+public class Calculator {
 
 	public static void main(String[] args) {
 		
-		System.out.println("계산식을 입력하시오.");
-		
+		System.out.println("계산식을 입력하세요.");
+		Calculate cal = new Calculate();
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int num1 = scan.nextInt();
 		String operator = scan.next();
 		int num2 = scan.nextInt();
-		
+
 		int result = 0;
-		
-		Calculate cal = new Calculate();
-		
-		if (operator.equals("+")){
+
+		if (operator.equals("+")) {
 			result = cal.add(num1, num2);
 			System.out.println(result);
 		}
-		if (operator.equals("-")){
+
+		if (operator.equals("-")) {
 			result = cal.sub(num1, num2);
 			System.out.println(result);
 		}
-		if (operator.equals("*")){
-			result  = cal.mul(num1, num2);
-			System.out.println(result);
-		}
-		if (operator.equals("/")){
-			cal.div(num1, num2);
+
+		if (operator.equals("*")) {
+			result = cal.mul(num1, num2);
 			System.out.println(result);
 		}
 
+		if (operator.equals("/")) {
+			result = cal.sub(num1, num2);
+			System.out.println(result);
+		}
+
+
 	}
+	
+
 }
