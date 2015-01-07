428618f7dd8cacdbb9ddebf30f32e8fb43c104ea
Kim <cckdycc@naver.com>
2015-01-07 19:45:29 +0900

    #3

--- src/com/study/main/Calculate.java
+++ src/com/study/main/Calculate.java
@@ -1,28 +1,100 @@
 package com.study.main;
 
+import java.util.Scanner;
+
 public class Calculate {
+
+	Scanner scan = new Scanner(System.in);
 
 	static int result;
 
-	public int add(int num1, int num2) {
+	public int add() {
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+
 		result = num1 + num2;
 		return result;
 	}
 
-	public int sub(int num1, int num2) {
+	public int sub() {
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
 		result = num1 - num2;
 		return result;
 	}
 
-	public int mul(int num1, int num2) {
+	public int mul() {
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
 		result = num1 * num2;
 		return result;
 	}
 
-	public int div(int num1, int num2) {
+	public int div() {
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
 		result = num1 - num2;
 		return result;
 	}
 
+	public String toHex() {
+
+		int decNumber = scan.nextInt();
+
+		char tempHexNumber[] = new char[20];
+		char remainder1[] = new char[20];
+		int qoutient = 1;
+		char remainder;
+		int i = 0;
+		int j = 0;
+
+		while (qoutient > 0) {
+			qoutient = decNumber / 16;
+			remainder = (char) (decNumber - 16 * qoutient);
+
+			switch (remainder) {
+
+			case 10:
+				remainder1[i] = 'A';
+				break;
+			case 11:
+				remainder1[i] = 'B';
+				break;
+			case 12:
+				remainder1[i] = 'C';
+				break;
+			case 13:
+				remainder1[i] = 'D';
+				break;
+			case 14:
+				remainder1[i] = 'E';
+				break;
+			case 15:
+				remainder1[i] = 'F';
+				break;
+			default:
+				break;
+			}
+
+			i++;
+		}
+
+		i = i - 1;
+
+		while (i >= 0) {
+			tempHexNumber[j] = remainder1[i];
+			i--;
+			j++;
+		}
+
+		String hexNumber = new String(tempHexNumber, 0, tempHexNumber.length);
+
+		return hexNumber;
+
+	}
 
 }
--- src/com/study/main/Calculator.java
+++ src/com/study/main/Calculator.java
@@ -5,35 +5,45 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		
-		System.out.println("계산식을 입력하세요.");
-		Calculate cal = new Calculate();
+		System.out.println("연산자를 선택하세요.");
+		System.out.println("더하기: 1 입력, 빼기: 2 입력, 곱하기: 3 입력, 나누기: 4 입력, 16진수로변환: 5 입력");
+		System.out.print(">>");
 
 		Scanner scan = new Scanner(System.in);
-
-		int num1 = scan.nextInt();
 		String operator = scan.next();
-		int num2 = scan.nextInt();
+		
+		System.out.println("피연산자를 입력하세요.");
+		
+		Calculate cal = new Calculate();
+
+		
 
 		int result = 0;
 
-		if (operator.equals("+")) {
-			result = cal.add(num1, num2);
+		if (operator.equals("1")) {
+			result = cal.add();
 			System.out.println(result);
 		}
 
-		if (operator.equals("-")) {
-			result = cal.sub(num1, num2);
+		if (operator.equals("2")) {
+			result = cal.sub();
 			System.out.println(result);
 		}
 
-		if (operator.equals("*")) {
-			result = cal.mul(num1, num2);
+		if (operator.equals("3")) {
+			result = cal.mul();
 			System.out.println(result);
 		}
 
-		if (operator.equals("/")) {
-			result = cal.sub(num1, num2);
+		if (operator.equals("4")) {
+			result = cal.sub();
 			System.out.println(result);
+		}
+		
+		if (operator.equals("5")) {
+			String result1 = new String();
+			result1 = cal.toHex();
+			System.out.println(result1);
 		}
 		
 
